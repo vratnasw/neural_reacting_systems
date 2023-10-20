e06604d72696b30645152d697bc4d2d779f6be66
Vish Ratnaswamy <21269712+vratnasw@users.noreply.github.com>
2023-10-19 23:53:41 -0700

    Add files via upload
    updated utilities

--- utils_nrl.py
+++ utils_nrl.py
@@ -13,7 +13,6 @@ def softmax(x, axis=1):
 
 
 def sample_gumbel(shape, eps=1.0e-10):
-
 	u = torch.rand(shape).float()
 	sample =  -torch.log(eps-torch.log(U+eps))
 	return sample
@@ -23,3 +22,21 @@ def gumbel_softmax_sample(logits, tau=1, eps=1.0e-10):
 	x = logits + Variable(gumbel_noise)
 	x = softmax(x/tau, axis=-1)
 	return x
+
+def sample_logistic(shape, eps=1.0e-10):
+	u =  torch.random(shape).float()
+	val = torch.log(u+eps) - torch.log(1-u+eps)
+	return val
+
+def gumble_softmax(logits, tau=1, hard = False, epsilon=1e-10):
+	input_soft = gumbel_softmax_sample(logits, tau, epsilon)
+	if hard:
+		shape = logits.size()
+		_, i = input_soft.data.max(-1)
+		input_hard = torch.zeros(*shape)
+		input_hard = input_hard.zero_().scatter_(-1,i.view(shape[:-1]+(1,)),1.0)
+		out  =  Variable(input_hard-input_soft.data) + input_soft
+	else:
+		out = input_soft
+
+	return out
